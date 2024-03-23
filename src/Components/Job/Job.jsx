72a33759178286b9368ea2bd3cdd80a625778bcd
mahammadshariaralamsarker <154776315+mahammadshariaralamsarker@users.noreply.github.com>
2024-03-23 12:36:09 +0600

    display job

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const Job = ({job}) => {
-    const {id, company, logo, featured, position, role, level, postedAt, contract, location, languages, tools} = job
+const Job = ({ job }) => {
+    const { id, company_name, logo, job_title, remote_or_onsite, location, job_type, salary, job_description, job_responsibility, educational_requirements, experiences} = job
     return (
-        <div>
-        <img src={logo} alt="" />
-            <h1>This is Job:{id}</h1>
+        <div className="card card-compact  bg-base-100 shadow-xl">
+            <figure><img src={logo} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{job_title}!</h2>
+                <p>{company_name}</p>
+                <div>
+                    <button className='px-5 mr-4 py-2 font-extrabold border-1  border text-[#7E90FE]'>{remote_or_onsite}</button>
+                    <button className='px-5 py-2 font-extrabold border-1  border text-[#7E90FE]'>{job_type}</button>
+                </div>
+                <div className="card-actions">
+                    <button className="btn btn-primary">View Details</button>
+                </div>
+            </div>
         </div>
     );
 };
